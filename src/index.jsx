84f8b15058f180671e68f872239b8bf781433ace
jamesfoday <jamesfodayofficial@gmail.com>
2025-04-07 11:46:21 +0200

    update
    bootstrap.min.css removed

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 import "./index.scss";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
+
+
 
 
 
 const App = () => {
     return (
-        <Container>
-            <MainView />
-        </Container>
+
+        <MainView />
     );
 };
 
